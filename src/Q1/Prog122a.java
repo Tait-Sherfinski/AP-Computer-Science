9599a8fe42f8252e0abfdeb59bd70e25bb445980
Tait-Sherfinski <Tsherfin@gmail.com>
2023-09-25 11:23:08 -0500

    update 122a

--- src/Q1/Prog122a.java
+++ src/Q1/Prog122a.java
@@ -2,12 +2,11 @@ package Q1;
 
 public class Prog122a {
     public static void main(String[] args) {
-        int lcv = 0;
-        int lcv2 = 0;
-        while (lcv < 50) {
-        lcv += 1;
-        lcv2 *= lcv;
-        System.out.println(lcv + " " + lcv2);
+        for (int lcv = 0; lcv < 50; lcv++) {
+            int col1 = lcv+1;
+            double col2 = col1 * col1;
+            double col3 = Math.sqrt(col1);
+            System.out.println("\nNumber: " + col1 + "\n\nSquared: " + col2 + "\n\nSquare Root: " + col3);
+        }
         }
     }
-}
