696b2539d39a3f934b42c18b4ee0e3e6fa7b76a5
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2023-10-04 01:30:06 +0000

    update

--- src/Q1/LP46.java
+++ src/Q1/LP46.java
@@ -4,11 +4,14 @@ import java.util.*;
 
 public class LP46 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int userAns = input.nextInt();
+
+        int ans = 0;
+        
         Random num1 = new Random();
         int showMe = num1. nextInt(10);
         Random num2 = new Random();
         int showMe2 = num2. nextInt(10);
-        System.out.println("What is " + showMe + " * " + showMe2);
-        int ans = showMe * showMe2;
     }
 }
