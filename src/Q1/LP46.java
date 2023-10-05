e6f7b5171361c75a3505d79a9c25c896b66211bc
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2023-10-05 00:42:36 +0000

    update

--- src/Q1/LP46.java
+++ src/Q1/LP46.java
@@ -5,15 +5,46 @@ import java.util.*;
 public class LP46 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int userAns = input.nextInt();
 
-        int ans = 0;
-        
-        Random num1 = new Random();
-        int showMe = num1. nextInt(10);
-        Random num2 = new Random();
-        int showMe2 = num2. nextInt(10);
+      Random r = new Random();
+      Random rand = new Random();
 
+      int a = 10;
+      int b = 1;
 
+      char operator = '?';
+      double ans = 0;
+
+      int num1 = rand.nextInt(11);
+      int num2 = rand.nextInt(11);
+
+      int max = Math.max(num1, num2);
+      int min = Math.min(num1, num2);
+
+      switch (r.nextInt(4)) {
+        case 0: operator = '+';
+          ans = max + min;
+          break;
+        case 1: operator = '-';
+          ans = max - min;
+          break;
+        case 2: operator = '*';
+          ans = max * min;
+          break;
+        case 3: operator = '/';
+          ans = max / min;
+          break;
+        default: operator = '?';
+      }
+
+      System.out.println(max + " " + operator + " " + min + " = ");
+      
+      double userans = input.nextDouble();
+
+      if (userans == ans) {
+        System.out.println("Correct");
+      } else {
+        System.out.println("Incorrect");
+      }
     }
 }
