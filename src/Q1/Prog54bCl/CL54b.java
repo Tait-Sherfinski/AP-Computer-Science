2c4e28930e1b104ae599edb57410743097587112
Tait-Sherfinski <Tsherfin@gmail.com>
2023-10-10 11:24:59 -0500

    update 10/10

---
+++ src/Q1/Prog54bCl/CL54b.java
@@ -0,0 +1,13 @@
+package Q1.Prog54bCl;
+
+public class CL54b {
+    private int Num1;
+    private int Num2;
+    private int Num3;
+    private int Num4;
+    private int Sum;
+
+    public CL54b(int num1, int num2, int num3, int num4) {
+        num1 = Num1;
+    }
+}
---
+++ src/Q1/Prog54bCl/Prog54bCl.java
@@ -0,0 +1,5 @@
+package Q1.Prog54bCl;
+
+public class Prog54bCl {
+
+}
