dcf0c696d39e6430e3aa43a59cd7a1808762a0e0
Tait-Sherfinski <Tsherfin@gmail.com>
2023-11-07 11:22:58 -0600

    update 402a

---
+++ src/Q2/Prog402aArray/Cl402a.java
@@ -0,0 +1,15 @@
+package Q2.Prog402aArray;
+
+public class Cl402a {
+    private double myId;
+    private double myScore;
+    private double myDiff;
+    private double myAvg;
+
+    public Cl402a(double id, double score) {
+        myId = id;
+        myScore = score;
+        myDiff = 0;
+        myAvg = 0;
+    }
+}
