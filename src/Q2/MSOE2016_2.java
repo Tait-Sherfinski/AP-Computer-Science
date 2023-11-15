8eca0481bfcb15f77f8a90a542b045c31e5859cf
Tait-Sherfinski <Tsherfin@gmail.com>
2023-11-15 11:33:29 -0600

    update 11/15

---
+++ src/Q2/MSOE2016_2.java
@@ -0,0 +1,14 @@
+package Q2;
+
+import java.sql.SQLOutput;
+import java.util.*;
+
+public class MSOE2016_2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter length of side A: ");
+        int sideA = input.nextInt();
+        System.out.print("Enter the length of side C: ");
+        int sideC = input.nextInt();
+    }
+}
