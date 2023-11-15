a01f127bb93b6b036a15d8c44431ba05794aa9fa
Tait-Sherfinski <Tsherfin@gmail.com>
2023-11-15 11:27:13 -0600

    update 11/15

--- src/Q2/BiggArray.java
+++ src/Q2/BiggArray.java
@@ -67,5 +67,94 @@ public class BiggArray {
         int myThird = array[2];
         array[2] = 5;
         System.out.println("The number that was ousted is " + myThird);
+
+        // 10
+        for (int num : array) {
+            if (num > 50 && num <= 59)
+                System.out.print(num);
+        }
+        System.out.println();
+
+        // 11
+        for (int lcv = 0; lcv < array.length; lcv++) {
+            if (array[lcv] % 4 == 0)
+                System.out.print(array[lcv] + " ");
+        }
+        System.out.println();
+
+        // 12
+        boolean sixty = false;
+        for (int temp : array) {
+            if (temp == 60) sixty = true;
+        }
+        System.out.println("Is 60 in the list? " + sixty);
+
+        // 13
+        boolean same = true;
+        for (int lcv = 0; lcv < array.length; lcv++) {
+            if (array[lcv] != array[array.length - 1 - lcv])
+                same = false;
+        }
+        System.out.println("Is the array palindromic? " + same);
+
+        // 14
+        double average = 0;
+        for (int x : array) {
+            average += x;
+        }
+        average /= array.length;
+
+        int count = 0;
+        for (int x : array)
+            if (x > average) count++;
+        System.out.printf("There are %d numbers greater than the average\n", count);
+
+        // 15
+        int evens = 0;
+        for ( int num : array)
+            if (num % 2 == 0) evens++;
+        System.out.printf("There are %d even numbers\n", evens);
+
+        // 16
+        int[] array2 = new int[array.length];
+        for (int lcv = 0; lcv < array.length; lcv++) {
+            array2[lcv] = array[array.length - 1 - lcv];
+        }
+
+        for (int n : array2)
+            System.out.print(n + " ");
+        System.out.println();
+
+        // 17
+        int[] shiftArr = circularShiftRight(array);
+        for (int n : shiftArr)
+            System.out.print(n + " ");
+        System.out.println();
+
+        // 18
+        int sum = 0;
+        for (int num : array) {
+            int digitsum = 0;
+            int temp = num;
+            while (temp > 0) {
+                int n = temp % 10;
+                temp /= 10;
+                digitsum += n;
+            }
+            sum += digitsum;
+        }
+        System.out.println("The sum of all digits of all elements is: " + sum);
     }
+
+    public static int[] circularShiftRight(int[] arr) {
+        int temp = arr[arr.length - 1];
+        int[] shifted = new int[arr.length];
+        for (int lcv = 1; lcv < arr.length; lcv++)
+            shifted[lcv] = arr[lcv - 1];
+        shifted[0] = 10;
+        return shifted;
+    }
+
+
+
 }
--- src/Q2/MSOE2016_1.java
+++ src/Q2/MSOE2016_1.java
@@ -5,12 +5,18 @@ import java.util.*;
 public class MSOE2016_1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter a word");
+        System.out.print("Enter a lowercase word: ");
         String word = input.nextLine();
+        String reverse = "";
+        Boolean same = false;
 
-        StringBuilder newWord = new StringBuilder();
-        newWord.append(word);
-        newWord = newWord.reverse();
+        for (int lcv = word.length() - 1; lcv >= 0; lcv--) {
+            reverse = reverse + word.charAt(lcv);
+        }
 
+        if (reverse.equals(word)) same = true;
+        else same = false;
+
+        System.out.println("Is the word palindromic? " + same);
     }
 }
