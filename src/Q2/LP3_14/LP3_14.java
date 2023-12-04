03b0afb5d93f9caaa7ac87b98103a175bb00c0cd
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2023-12-04 17:21:44 +0000

    update 12/4

---
+++ src/Q2/LP3_14/LP3_14.java
@@ -0,0 +1,12 @@
+package Q2.LP3_14;
+
+public class LP3_14 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Election Results for New York: ");
+    System.out.print("Awbrey: ");
+    int aNY = input.nextInt();
+    System.out.print("Martinez: ");
+    int mNY = input.nextInt();
+  }
+}
