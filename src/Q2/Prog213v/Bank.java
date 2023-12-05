9beb01fe6623bc84c8511af87d36cc4e0864c518
Tait-Sherfinski <Tsherfin@gmail.com>
2023-12-05 11:02:27 -0600

    update

---
+++ src/Q2/Prog213v/Bank.java
@@ -0,0 +1,11 @@
+package Q2.Prog213v;
+
+public class Bank {
+    private double myAmount;
+
+    public Bank(double amount) {
+        myAmount = amount;
+    }
+
+    public double getAmount() { return myAmount; }
+}
