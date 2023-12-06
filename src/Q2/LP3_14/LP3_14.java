a97099d8fd60a2ca93f53cd663599e12147a8048
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2023-12-06 17:02:06 +0000

    update

--- src/Q2/LP3_14/LP3_14.java
+++ src/Q2/LP3_14/LP3_14.java
@@ -10,5 +10,24 @@ public class LP3_14 {
     int aNY = input.nextInt();
     System.out.print("Martinez: ");
     int mNY = input.nextInt();
+    System.out.println("Election Results for New Jersey");
+    System.out.print("Awbrey: ");
+    int aNJ = input.nextInt();
+    System.out.print("Martinez: ");
+    int mNJ = input.nextInt();
+    System.out.println("Election Results for Connecticut");
+    System.out.print("Awbrey: ");
+    int aC = input.nextInt();
+    System.out.print("Martinez: ");
+    int mC = input.nextInt();
+
+    ClLP3_14 wow = new ClLP3_14();
+    wow.calc();
+
+    int aVotes = wow.getAVotes();
+    int mVotes = wow.getMVotes();
+    int total = wow.getTotal();
+
+    System.out.println("Candidates\t\tVotes\tPercentage\nAwbrey\t\t\t" + aVotes + "\t" + aVotes / total * 100 + "%\nMartinez\t\t" + mVotes + "\t" + mVotes);
   }
 }
