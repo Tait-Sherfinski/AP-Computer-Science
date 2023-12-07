34f353dc06fe332c8c3bcd69d3b95b3f06d7a0ac
Tait-Sherfinski <Tsherfin@gmail.com>
2023-12-07 11:22:52 -0600

    update 12/7

--- src/Q2/Prog214c/Cl214c.java
+++ src/Q2/Prog214c/Cl214c.java
@@ -1,4 +1,41 @@
 package Q2.Prog214c;
 
 public class Cl214c {
+    private String gasType;
+    private double gallons;
+    private double carWash;
+    private double total;
+
+    public Cl214c(String gT, double gl) {
+        gasType = gT;
+        gallons = gl;
+        carWash = 2.0;
+        total = 0.0;
+    }
+
+    public void calc() {
+        if (gasType == "R") {
+            if (gallons < 20 && gallons > 10) {
+                total = (gallons * 1.359) + carWash * ((gallons - 10) * 0.1);
+            } else if (gallons > 20) {
+                total = (gallons * 1.359);
+            } else total = (gallons * 1.359) + carWash;
+        }
+        else if (gasType == "P") {
+            if (gallons < 20 && gallons > 10) {
+                total = (gallons * 1.479) + carWash * ((gallons - 10) * 0.1);
+            } else if (gallons > 20) {
+                total = (gallons * 1.479);
+            } else total = (gallons * 1.479) + carWash;
+        }
+        else if (gasType == "H") {
+            if (gallons < 20 && gallons > 10) {
+                total = (gallons * 1.429) + carWash * ((gallons - 10) * 0.1);
+            } else if (gallons > 20) {
+                total = (gallons * 1.429);
+            } else total = (gallons * 1.429) + carWash;
+        }
+    }
+
+    public double getTotal() { return total; }
 }
--- src/Q2/Prog214c/Prog214c.java
+++ src/Q2/Prog214c/Prog214c.java
@@ -3,14 +3,26 @@ package Q2.Prog214c;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Prog214c {
     public static void main(String[] args) {
         try {
             Scanner input = new Scanner(new File("Langdat/prog214c.dat"));
-
+            ArrayList<Cl214c> list = new ArrayList<>();
+            
+            
+            
+            String gT = input.nextLine();
+            double gl = input.nextDouble();
+            Cl214c wow = new Cl214c(gT, gl);
+            
+            double total = wow.getTotal();
+            
             while (input.hasNext()) {
+                for (int lcv = 0; lcv < list.size(); lcv++) {
 
+                }
             }
         } catch (IOException e) {
             System.out.println("Can't find data file");
