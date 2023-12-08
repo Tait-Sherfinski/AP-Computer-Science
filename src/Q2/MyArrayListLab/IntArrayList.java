8e5d3bd48aacd64dabcf385508ea1d613b3bae29
Tait-Sherfinski <Tsherfin@gmail.com>
2023-12-08 11:21:44 -0600

    update 12/8

---
+++ src/Q2/MyArrayListLab/IntArrayList.java
@@ -0,0 +1,51 @@
+package Q2.MyArrayListLab;
+
+public class IntArrayList {
+    private int[] myArray;
+    private int mySize;
+    private int myCapacity;
+    private final int DEFAULT_SIZE = 16;
+
+    public IntArrayList() {
+        myArray = new int[DEFAULT_SIZE];
+        mySize = 0;
+        myCapacity = DEFAULT_SIZE;
+    }
+
+    private void doubleCapacity() {
+        myCapacity *= 2;
+        int[] temp = new int[myCapacity];
+        for (int lcv = 0; lcv < mySize; lcv++)
+            temp[lcv] = myArray[lcv];
+        myArray = temp;
+    }
+
+    private void halfCapacity() {
+        myCapacity /= 2;
+        int[] temp = new int[myCapacity];
+        for (int lcv = 0; lcv < mySize; lcv++)
+            temp[lcv] = myArray[lcv];
+        myArray = temp;
+    }
+
+    public void add(int num) {
+        if (mySize + 1 == myCapacity) {
+            doubleCapacity();
+        }
+        myArray[mySize] = num;
+        mySize++;
+    }
+
+    public void remove(int index) {
+        for (int lcv = index+1; lcv < mySize; lcv++) {
+            myArray[lcv-1] = myArray[lcv];
+        }
+        mySize--;
+        if (mySize <= (myCapacity * 0.25)) {
+            halfCapacity();
+        }
+    }
+
+    public int get(int index) { return myArray[index]; }
+    public void set(int index, int num) {  }
+}
--- src/Q2/Prog214c/Prog214c.java
+++ src/Q2/Prog214c/Prog214c.java
@@ -9,20 +9,16 @@ public class Prog214c {
     public static void main(String[] args) {
         try {
             Scanner input = new Scanner(new File("Langdat/prog214c.dat"));
-            ArrayList<Cl214c> list = new ArrayList<>();
-            
-            
-            
+
             String gT = input.nextLine();
             double gl = input.nextDouble();
             Cl214c wow = new Cl214c(gT, gl);
             
             double total = wow.getTotal();
+            System.out.println("Total: " + total);
             
             while (input.hasNext()) {
-                for (int lcv = 0; lcv < list.size(); lcv++) {
 
-                }
             }
         } catch (IOException e) {
             System.out.println("Can't find data file");
