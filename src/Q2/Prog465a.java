bfa5bbf8212669b61f7a192e6d57da18b9ba803a
Tait-Sherfinski <Tsherfin@gmail.com>
2024-01-02 11:23:07 -0600

    update 1/2

---
+++ src/Q2/Prog465a.java
@@ -0,0 +1,19 @@
+package Q2;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Prog465a {
+    public static void main(String[] args) {
+        try {
+            Scanner input = new Scanner(new File("Langdat/prog285b.dat"));
+
+            while (input.hasNext()) {
+
+            }
+        } catch (IOException e) {
+            System.out.println("Can't find data file");
+        }
+    }
+}
