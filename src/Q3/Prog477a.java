7342a4d79621496362e4973dc54516630a826141
Tait-Sherfinski <Tsherfin@gmail.com>
2024-01-22 11:14:53 -0600

    update 1/22

---
+++ src/Q3/Prog477a.java
@@ -0,0 +1,17 @@
+package Q3;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Prog477a {
+    public static void main(String[] args) {
+        try {
+            Scanner input = new Scanner(new File("Langdat/prog285b.dat"));
+
+
+        } catch (IOException e) {
+            System.out.println("Can't find data file");
+        }
+    }
+}
