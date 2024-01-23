b73f98357bc5ed56ecabe1dd0417bb0becd9d90e
Tait-Sherfinski <Tsherfin@gmail.com>
2024-01-23 11:22:13 -0600

    update 1/23

--- src/Q3/Prog477a.java
+++ src/Q3/Prog477a.java
@@ -7,8 +7,26 @@ import java.util.Scanner;
 public class Prog477a {
     public static void main(String[] args) {
         try {
-            Scanner input = new Scanner(new File("Langdat/prog285b.dat"));
+            Scanner input = new Scanner(new File("Langdat/prog477a.dat"));
 
+            int[][] mat = new int[4][5];
+            for (int row = 0; row < mat.length; row++) {
+                for (int col = 0; col < mat[0].length; col++) {
+                    mat[row][col] = input.nextInt();
+                }
+            }
+
+            for (int[] col : mat) {
+                for (int j : col) {
+
+                }
+            }
+
+            for (int[] row : mat) {
+                for (int n : row)
+                    System.out.print(n + "\t");
+                System.out.println();
+            }
 
         } catch (IOException e) {
             System.out.println("Can't find data file");
