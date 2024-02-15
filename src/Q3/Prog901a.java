1641c39d6b5bd93869ce885ce1772f2da2bf6995
Tait-Sherfinski <Tsherfin@gmail.com>
2024-02-15 11:21:23 -0600

    update 2/15

---
+++ src/Q3/Prog901a.java
@@ -0,0 +1,34 @@
+package Q3;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Prog901a {
+
+    public static void printFile(Scanner input) {
+        System.out.println(input);
+    }
+
+    public static String reverseString(String s) {
+        s = "i";
+        for (int i = s.length(); i > 0; i--) {
+            for (int j = 0; j < s.length(); j++) {
+                int count = 0;
+                int temp = i;
+                i = j;
+                j = temp;
+
+            }
+        } return s;
+    }
+
+    public static void main(String[] args) {
+        try {
+            Scanner input = new Scanner(new File("Langdat/prog512h.dat"));
+
+        } catch (IOException e) {
+            System.out.println("Can't find data file");
+        }
+    }
+}
