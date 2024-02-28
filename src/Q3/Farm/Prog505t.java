9964c105e28877d27ba383806fe3e26fac1a2511
Tait-Sherfinski <Tsherfin@gmail.com>
2024-02-28 11:34:01 -0600

    update 2/28

--- src/Q3/Farm/Prog505t.java
+++ src/Q3/Farm/Prog505t.java
@@ -2,12 +2,16 @@ package Q3.Farm;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Prog505t {
     public static void main(String[] args) {
         try {
             Scanner input = new Scanner(new File("Langdat/prog505t.dat"));
+
+            ArrayList<Cow> cow = new ArrayList<Cow>();
+            ArrayList<Horse> horse = new ArrayList<Horse>();
+
 
             int hayBales = input.nextInt();
             double hayCost = input.nextDouble();
@@ -23,6 +27,19 @@ public class Prog505t {
                 hayBales -= hayEaten;
                 cornCobs -= cornEaten;
             }
+
+            int horses = input.nextInt();
+            for (int i = 0; i < horses; i++) {
+                int weight = input.nextInt();
+                int hayEaten = input.nextInt();
+                int cornEaten = input.nextInt();
+                int rides = input.nextInt();
+                double rideCost = input.nextDouble();
+                hayBales -= hayEaten;
+                cornCobs -= cornEaten;
+            }
+
+
         } catch (IOException e) {
             System.out.println("Can't find data file");
         }
