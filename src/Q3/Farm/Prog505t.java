bfb9119c06bad085e76f2e1db0f73218ebc02945
Tait-Sherfinski <Tsherfin@gmail.com>
2024-03-01 11:22:21 -0600

    update 3/1

--- src/Q3/Farm/Prog505t.java
+++ src/Q3/Farm/Prog505t.java
@@ -9,6 +9,7 @@ public class Prog505t {
         try {
             Scanner input = new Scanner(new File("Langdat/prog505t.dat"));
 
+            List<Animal1> animals = new ArrayList<Animal1>();
             List<Cow1> cow = new ArrayList<Cow1>();
             List<Horse1> horse = new ArrayList<Horse1>();
 
@@ -26,9 +27,9 @@ public class Prog505t {
                 int milkLBs = input.nextInt();
                 int hayEaten = input.nextInt();
                 int cornEaten = input.nextInt();
-                cIncome = milkLBs * 0.20;
                 Cow1 wow = new Cow1(weight, milkLBs, cornEaten, hayEaten);
                 cow.add(wow);
+                animals.add(wow);
                 hayBales -= hayEaten;
                 cornCobs -= cornEaten;
             }
@@ -40,16 +41,49 @@ public class Prog505t {
                 int cornEaten = input.nextInt();
                 int rides = input.nextInt();
                 double rideCost = input.nextDouble();
-                hIncome = rides * rideCost;
                 Horse1 fred = new Horse1(weight, cornEaten, hayEaten, rides, rideCost);
                 horse.add(fred);
+                animals.add(fred);
                 hayBales -= hayEaten;
                 cornCobs -= cornEaten;
             }
 
-            double totalIncome = cIncome + hIncome;
+            int foodCost = 0;
+            int totWeight = 0;
+            int totAnimals = animals.size();
+            for (int lcv = 0; lcv < totAnimals; lcv++) {
+                if (animals.get(lcv) instanceof Horse1) {
+                    Horse1 horse1 = (Horse1)animals.get(lcv);
+                    hIncome += (horse1.getRideCost() * horse1.getRides());
+                    foodCost += (horse1.getNumCorn() * cornCost) + (horse1.getNumHayBales() * hayCost);
+                    totWeight += horse1.getWeight();
+                }
+            }
 
-            System.out.println(totalIncome);
+            for (int lcv = 0; lcv < totAnimals; lcv++) {
+                if (animals.get(lcv) instanceof Cow1) {
+                    Cow1 cow1 = (Cow1)animals.get(lcv);
+                    cIncome += (cow1.getMilk() * cow1.getMilkPrice());
+                    foodCost += (cow1.getNumCorn() * cornCost) + (cow1.getNumHayBales() * hayCost);
+                    totWeight += cow1.getWeight();
+                }
+            }
+
+            Cow1 james = new Cow1(1250, 80, 4, 3);
+            animals.set(0, cow.get(0));
+
+            double totIncome = (cIncome + hIncome);
+
+            System.out.println("The total cost to feed all animals is $" + foodCost);
+            System.out.println("The total income of all animals is $" + totIncome);
+            System.out.println("The total weight of all animals is : " + totWeight);
+            if (hayBales > 0 && cornCobs > 0) {
+                System.out.println("There is enough food to feed all animals");
+                System.out.println("There are " + hayBales + " hay bales left");
+                System.out.println("There are " + cornCobs + " corn cobs left");
+            }
+            else System.out.println("There is not enough food to feed all animals");
+
             System.out.println("The number of cows and horses is: " + (cows + horses));
 
 
