6131afe3e7516e1894feaf6a60f1d84e9b00cda1
Tait-Sherfinski <Tsherfin@gmail.com>
2024-04-01 11:24:08 -0500

    update 4/1

---
+++ src/Q3/MSOE2019one.java
@@ -0,0 +1,20 @@
+package Q3;
+
+import java.util.*;
+
+public class MSOE2019one {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter two letters: ");
+        String two = input.nextLine();
+        System.out.print("Enter a line of text: ");
+        String line = input.nextLine();
+
+        int count = 0;
+
+        for (int i = 0; i < line.length(); i++) {
+            if (line.substring(i, i+1).equals(two.substring(0, 1))) count++;
+        }
+        System.out.println(count);
+    }
+}
