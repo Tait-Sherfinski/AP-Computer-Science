b3ba3d2b8a0adae10f1c1f17c81561681e23fe20
Tait-Sherfinski <Tsherfin@gmail.com>
2024-04-03 11:33:28 -0500

    update 4/3

---
+++ src/Q3/MSOE2021one.java
@@ -0,0 +1,20 @@
+package Q3;
+
+import java.util.*;
+
+public class MSOE2021one {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a letter: ");
+        String let = input.nextLine();
+        System.out.print("Enter a sentence: ");
+        String sent = input.nextLine();
+
+        for (int i = 0; i < sent.length(); i++) {
+            String word = "";
+            if (sent.substring(i, i+1).equals(let + " ")) {
+
+            }
+        }
+    }
+}
