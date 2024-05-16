d7a82740b287191d73d210d59a2e187d14dcb5f9
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2024-05-16 11:23:19 -0500

    update 5/16

---
+++ src/Q4/Prog7000l/LinearRegression.java
@@ -0,0 +1,46 @@
+package Q4.Prog7000l;
+
+import java.util.Arrays;
+public class LinearRegression {
+    private double theta0;
+    private double theta1;
+    private double alpha;
+    private double tolerance;
+    private double[] objective;
+
+    public LinearRegression(double thetaOne, double thetaZero, double learningRate, double tol) {
+        theta0 = thetaZero;
+        theta1 = thetaOne;
+        alpha = learningRate;
+        tolerance = tol;
+    }
+
+    public void fit(double[][] x_train, double[] y_train, int epochs) {
+        int m = x_train.length;
+        objective = new double[epochs];
+        double[] e = new double[m];
+
+        for (int epoch = 0; epoch < epochs; epoch++) {
+            if (epoch > 0) {
+                double temp0 = 0, temp1 = 0;
+                for (int i = 0; i < m; i++) {
+                    double temp = (theta1 * x_train[i][0] + theta0) - y_train[i];
+                    temp0 += temp;
+                    temp1 += temp * x_train[i][0];
+                }
+                theta0 -= (1.0/m) * alpha * temp0;
+                theta1 -= (1.0/m) * alpha * temp0;
+            }
+        }
+    }
+
+    public String toString() {
+        return "Regression line: y = " + theta1 + " * x + " + theta0 + "\nObjective: " +
+                Arrays.toString(objective) + "\nMin. Objective: " +
+                (double)Arrays.stream(objective).min().getAsDouble();
+    }
+
+    public double getIntercept() { return theta0; }
+    public double getSlope() { return theta1; }
+    public double predict(double x) { return theta1 * x + theta0; }
+}
